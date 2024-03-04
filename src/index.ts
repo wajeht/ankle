940d7db481cfc5d7214227d243f79dfd290686a6
wajeht <58354193+wajeht@users.noreply.github.com>
2024-03-03 18:29:27 -0600

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
+import fs from 'fs';
+import ejs from 'ejs';
+import path from 'path';
+import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
+import marked from 'marked';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			...helmet.contentSecurityPolicy.getDefaultDirectives(),
+			'default-src': ["'self'", 'plausible.jaw.dev'],
+			'script-src': ["'self'", "'unsafe-inline'", 'plausible.jaw.dev'],
+		},
+	}),
+);
+
+app.use(cors());
+app.use(compression());
+app.use(express.static(path.join(process.cwd(), 'public'), { maxAge: '24h' }));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views', 'pages'));
+app.set('layout', path.join(__dirname, 'views', 'layouts', 'main.html'));
+app.use(expressLayouts);
+
+app.get('/healthz', (req, res) => res.status(200).send('ok'));
+
+app.get('/', (req, res) => {
+	fs.readdir(path.join(__dirname, 'posts'), (err, files) => {
+		if (err) return res.status(500).send('Failed to load posts.');
+		const posts = files
+			.filter((file) => file.endsWith('.md'))
+			.map((file) => {
+				const title = path.basename(file, '.md');
+				return { title, file };
+			});
+
+		res.render('posts.html', { title: 'posts', posts });
+	});
+});
+
+app.get('/:post', (req, res) => {
+	const postPath = path.join(__dirname, 'posts', `${req.params.post}.md`);
+	fs.readFile(postPath, 'utf8', (err, data) => {
+		if (err) return res.status(404).send('Post not found!');
+		return res.render('post.html', { title: req.params.post, content: marked.marked(data) });
+	});
+});
+
+app.use((req, res, _next) => res.status(404).send('not found'));
+
+app.use((err, req, res, next) => res.status(500).send('error'));
+
+const server = app.listen(PORT, () => {
+	console.log(`Server was started on http://localhost:${PORT}`);
+});
+
+function gracefulShutdown() {
+	console.log('Received kill signal, shutting down gracefully.');
+	server.close(() => {
+		console.log('HTTP server closed.');
+		process.exit(0);
+	});
+}
+
+process.on('SIGINT', gracefulShutdown);
+
+process.on('SIGTERM', gracefulShutdown);
+
+process.on('unhandledRejection', (reason, promise) => {
+	console.error('Unhandled Rejection at: ', promise, ' reason: ', reason);
+});
+
+export { app };
---
+++ src/posts/week-1.md
@@ -0,0 +1,7 @@
+---
+title: week 1
+date: 2023-02-26
+author: wajeht
+---
+
+Amet facilisi eget massa ad etiam sollicitudin
